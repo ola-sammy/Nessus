3f96d84565dd7cd9b5118fc6725e31ce68bde3b1
ola-sammy <139212029+ola-sammy@users.noreply.github.com>
2023-09-22 00:40:33 -0600

    Add files via upload

---
+++ install-tenable-nessus.sh
@@ -0,0 +1,5 @@
+sudo yum update -y
+sudo curl --request GET --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.6.1-es7.x86_64.rpm' --output 'Nessus-10.6.1-es7.x86_64.rpm'
+sudo rpm -ivh Nessus-10.6.1-es7.x86_64.rpm
+sudo systemctl enable nessusd
+sudo systemctl start nessusd
