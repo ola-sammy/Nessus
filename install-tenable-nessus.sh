a9faa1311c4c757c9389df486b550042af6e68c0
ola-sammy <139212029+ola-sammy@users.noreply.github.com>
2023-09-22 00:41:34 -0600

    Update install-tenable-nessus.sh

--- install-tenable-nessus.sh
+++ install-tenable-nessus.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo yum update -y
 sudo curl --request GET --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.6.1-es7.x86_64.rpm' --output 'Nessus-10.6.1-es7.x86_64.rpm'
 sudo rpm -ivh Nessus-10.6.1-es7.x86_64.rpm
